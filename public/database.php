1d160d18ad3f6302aa7e4eb55be7e502d2b8011b
frank-sons <frank.sons@code-quality.de>
2018-11-21 15:26:52 +0100

    fixed config and db example for container connection

--- public/database.php
+++ public/database.php
@@ -5,15 +5,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 /** @var array $dbConfig */
 $dbConfig = parse_ini_file(__DIR__ . '/../conf/db_conf.ini');
 
-var_dump($dbConfig);
-
-$host       = $dbConfig['host'] . ':' . $dbConfig['port'];
-$database   = $dbConfig['database'];
+$host       = 'host=' . $dbConfig['host'];
+$database   = 'dbname=' . $dbConfig['database'];
 $username   = $dbConfig['username'];
 $password   = $dbConfig['password'];
 
+$dsn = 'mysql:' . $database . ';' . $host;
+
 try {
-    $db = new PDO('mysql:host='.$host, $username, $password);
+    $db = new PDO($dsn, $username, $password);
+    print "DB Connection: Success!<br/>";
 } catch (PDOException $exception) {
     print "Error!: " . $exception->getMessage() . "<br/>";
     die();
